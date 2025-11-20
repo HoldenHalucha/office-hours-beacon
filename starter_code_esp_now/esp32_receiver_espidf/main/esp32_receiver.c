cf2a85bdfd4d6e810b5e510fef3263fb91d6c6d5
aspatil374 <123611751+aspatil374@users.noreply.github.com>
2025-11-20 15:59:48 -0500

    Update esp32_receiver.c
    changes to the receiver code with transmitter-receiver packet format

--- starter_code_esp_now/esp32_receiver_espidf/main/esp32_receiver.c
+++ starter_code_esp_now/esp32_receiver_espidf/main/esp32_receiver.c
@@ -8,6 +8,11 @@
 #include "esp_event.h"
 #include "esp_log.h"
 
+//for deep sleep
+#include "esp_sleep.h"
+#define MICROSEC_TO_sEC 1000000ULL
+#define DEEP_SLEEP_SECONDS 10
+
 #define LED_PIN 8
 
 // logic is inverted for super-mini boards for low power (probably)
@@ -15,55 +20,65 @@
 #define LED_HIGH 0
 
 // ESP-NOW/WiFi frequency and listening window
-uint16_t LISTEN_INTERVAL = 10000; //ms
-uint16_t LISTEN_WINDOW = 500; //ms
+uint16_t LISTEN_INTERVAL = 5000; //ms
+uint16_t LISTEN_WINDOW = 200; //ms
 
-// Global state for latest command
-typedef struct {
-    int position;
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-} receiver_state_t;
-
-static receiver_state_t g_state = {0, 0, 0, 0};
 
 // should hopefully call this when a message is received
 void esp_now_received(const esp_now_recv_info_t *message_received, const uint8_t *data, int len) {
     gpio_set_level(LED_PIN, LED_HIGH);
-    printf("From MAC: ");
+    printf("Received stuff from MAC: ");
     for (int i = 0; i < 6; i++) printf("%02X", message_received->src_addr[i]);
     printf(" | Length: %d | RSSI: %d\n", len, message_received->rx_ctrl->rssi);
 
-    // Expect payload like: "pos|r,g,b"
-    int pos = 0, r = 0, g = 0, b = 0;
-    if (len > 0) {
-        // Ensure null-terminated temporary buffer for parsing
-        char buf[64];
-        int copyLen = len < (int)sizeof(buf) - 1 ? len : (int)sizeof(buf) - 1;
-        for (int i = 0; i < copyLen; ++i) buf[i] = (char)data[i];
-        buf[copyLen] = '\0';
-
-        int matched = sscanf(buf, "%d|%d,%d,%d", &pos, &r, &g, &b);
-        if (matched == 4) {
-            if (r < 0) r = 0; if (r > 255) r = 255;
-            if (g < 0) g = 0; if (g > 255) g = 255;
-            if (b < 0) b = 0; if (b > 255) b = 255;
-            g_state.position = pos;
-            g_state.r = (uint8_t)r;
-            g_state.g = (uint8_t)g;
-            g_state.b = (uint8_t)b;
-            printf("Saved state -> pos:%d rgb:[%u,%u,%u]\n", g_state.position, g_state.r, g_state.g, g_state.b);
-        } else {
-            printf("Unexpected payload format: %s\n", buf);
-        }
-    }
+    //pretending to do something
+    vTaskDelay(20 / portTICK_PERIOD_MS);
 
     gpio_set_level(LED_PIN, LED_LOW);
+
+    for(int i = 0; i < len-1; ++i) {
+        printf("%c", data[i]);
+    }
+    //printf("this is the last character: %c\n", data[len-1]);
+    printf("\n");
+
+    //message received and is the right size
+    //idk why the message length shows up as 15 instead of 14
+    int red_val;
+    int green_val;
+    int blue_val;
+    int blink_mode;
+    int position;
+    if((len == 15) && (data[0] == 'S') && (data[13] == 'Z')) {
+        sscanf((char*)data + 1, "%3d", &red_val);
+        sscanf((char*)data + 4, "%3d", &green_val);
+        sscanf((char*)data + 7, "%3d", &blue_val);
+        sscanf((char*)data + 10, "%1d", &blink_mode);
+        sscanf((char*)data + 11, "%2d", &position);
+
+        printf("red: %d\n", red_val);
+        printf("green: %d\n", green_val);
+        printf("blue: %d\n", blue_val);
+        printf("blink_mode: %d\n", blink_mode);
+        printf("position: %d\n", position);
+    }
+
+    //message received but it is the wrong size, print error message
+    else {
+        printf("ERROR: Invalid message length or format. Possible loss of packets!!!\n");
+    }
+
+    printf("Goodbye\n");
+    esp_sleep_enable_timer_wakeup(DEEP_SLEEP_SECONDS * MICROSEC_TO_sEC);
+    esp_deep_sleep_start();
+
 }
 
 
 void app_init(void) {
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+    printf("Hello\n");
+
     // need this for esp-now
     ESP_ERROR_CHECK(nvs_flash_init());
 
@@ -81,9 +96,11 @@ void app_init(void) {
     // initialize ESP-NOW
     ESP_ERROR_CHECK(esp_now_init());
 
+    
     // power saving test 
-    esp_wifi_connectionless_module_set_wake_interval(LISTEN_INTERVAL); // how frequently to check
-    esp_now_set_wake_window(LISTEN_WINDOW); // how long to check
+    //esp_wifi_connectionless_module_set_wake_interval(LISTEN_INTERVAL); // how frequently to check
+    //esp_now_set_wake_window(LISTEN_WINDOW); // how long to check
+    
 
     //sanity for me
     printf("LET'S GET STARTED BOSS!\n");
